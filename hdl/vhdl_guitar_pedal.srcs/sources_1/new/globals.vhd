297d3812bfcffab52d091b387557338a13298b14
Leo Brown <leo.brown.lcrb@gmail.com>
2026-05-25 20:04:14 -0400

    bin shifter

--- hdl/vhdl_guitar_pedal.srcs/sources_1/new/globals.vhd
+++ hdl/vhdl_guitar_pedal.srcs/sources_1/new/globals.vhd
@@ -2,8 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package globals is
-	signal fft_len : integer := 1024;
-	signal depth : integer := 24;
+	constant fft_len : integer := 1024;
+	constant depth : integer := 24;
 	type freq_buffer is array (0 to fft_len-1) of std_logic_vector((depth*2)-1 downto 0);
 	type idx is array (0 to fft_len-1) of std_logic_vector(depth-1 downto 0);
 end;
--- hdl/vhdl_guitar_pedal.srcs/sources_1/new/pitch_shift.vhd
+++ hdl/vhdl_guitar_pedal.srcs/sources_1/new/pitch_shift.vhd
@@ -7,15 +7,15 @@ use work.all;
 entity pitch_shift is
     generic (
         fft_len : integer := 1024;
-        n : integer := 48;
-        m : integer := 8
+        n : integer := depth;
+        m : integer := 24
     );
     port (
         clk : in std_logic;
-        exp_in : in mem_t;
+        exp_in : in freq_buffer;
         idx_in : in idx;
-        shift_hertz : in std_logic_vector(M-1 downto 0);
-        exp_out : out mem_t;
+        shift : in std_logic_vector(M-1 downto 0);
+        exp_out : out freq_buffer;
         idx_out : out idx;
         delays : out integer
     );
@@ -23,12 +23,28 @@ end entity pitch_shift;
 
 architecture beh of pitch_shift is
 
-    signal re : idx;
-    signal im : idx;
+    type reals is array(0 to fft_len-1) of real;
+
+    signal re : reals;
+    signal im : reals;
+
+    signal mag : reals;
+    signal phase : reals;
+
+    signal re_t : reals;
+    signal im_t : reals;
 
 begin
 
     for i in 0 to fft_len-1 generate
+        re(i) <= real(exp_in(i)(depth-1 downto 0));
+        im(i) <= real(exp_in(i)((depth*2)-1 downto depth));
+
+        mag(i) <= sqrt((re(i)*re(i))+(im(i)*im(i)));
+        phase(i) <= arctan(im(i)/re(i)) + real(shift)/real((std_logic_vector(23 downto 0) <= (others => '1')));
+
+        re_t <= mag(i) * cos(phase(i));
+        im_t <= mag(i) * sin(phase(i));
     end generate;
 
 end architecture beh;
